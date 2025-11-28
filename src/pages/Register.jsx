6c6f6aba944d630e1e1bbf67d86b0f2ecbde517d
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2025-11-28 12:19:51 +0530

    button.jsx and label.jsx file created

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,15 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 import { useFormik } from "formik";
 import { useContext } from "react";
 import UserContext from "../context/UserContext";

6bea3189347fac6e6804bf63f3b4bfe30a5bab87
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2025-12-08 19:33:30 +0530

    VehicleList component created

--- src/pages/VehicleList.jsx
+++ src/pages/VehicleList.jsx
@@ -1,7 +1,73 @@
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../context/UserContext";
+import axios from "../config/axios";
+
+"use client"
+
+import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DataTableColumnHeader } from "../components/data-table-column-header";
+import { useSelector } from "react-redux";
+import { DataTable } from "@/components/data-table";
+import { SidebarProvider } from "../components/ui/sidebar";
+import { AppSidebar } from "../components/app-sidebar";
+
+export const columns = [
+  { accessorKey: "_id", header: "ID" },
+  {
+    accessorKey: "username",
+    header: ({ column }) => {
+      <DataTableColumnHeader column={column} title="Name" />
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      <DataTableColumnHeader column={column} title="Email" />
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  { accessorKey: "role", header: "Role" },
+  { accessorKey: "license", header: "License" },
+  { accessorKey: "action", header: "Action" }
+]
+
 export default function VehicleList() {
+    const { data } = useSelector((state) => {
+      return state.booking;
+    })
     return(
-        <div>
-            
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <main className="p-4">
+              <DataTable columns={columns} data={data} />
+            </main>
+        </SidebarProvider>
     )
 }
--- src/reducers/bookingReducer.jsx
+++ src/reducers/bookingReducer.jsx
@@ -1,9 +1,9 @@
 export default function bookingSlice(action, state) {
     switch(action.type) {
-        case SET_BOOKING: {
+        case "SET_BOOKING": {
             return { ...state, booking: action.payload }
         }
-        case SET_SERVER_ERROR: {
+        case "SET_SERVER_ERROR": {
             return { ...state, serverErrMsg: action.payload }
         }
         default: {

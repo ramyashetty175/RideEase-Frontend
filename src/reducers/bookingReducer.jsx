ca9279ef819b75a05d1881661f450e94426d4841
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2025-12-08 19:28:09 +0530

    bookingReducer created

---
+++ src/reducers/bookingReducer.jsx
@@ -0,0 +1,13 @@
+export default function bookingSlice(action, state) {
+    switch(action.type) {
+        case SET_BOOKING: {
+            return { ...state, booking: action.payload }
+        }
+        case SET_SERVER_ERROR: {
+            return { ...state, serverErrMsg: action.payload }
+        }
+        default: {
+            return state;
+        }
+    }
+}

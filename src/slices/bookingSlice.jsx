f0ee4cb253167a62a2002e8c8f45e8b230f3d810
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2025-12-09 15:53:59 +0530

    remove booking and update booking created on bookingSlice

--- src/slices/bookingSlice.jsx
+++ src/slices/bookingSlice.jsx
@@ -1,12 +1,47 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../config/axios";
 
-export const fetchBooking = createAsyncThunk("booking/fetchBooking", async(undefined, { rejectWithValue }) => {
+export const fetchBooking = createAsyncThunk("booking/fetchBooking", async (undefined, { rejectWithValue }) => {
     try {
         const response = await axios.get('/api/bookings', { headers: { Authorization: localStorage.getItem('token')}});
         return response.data;
     } catch(err) {
         console.log(err);
+    }
+})
+
+export const createBooking = createAsyncThunk("booking/createBooking", async ({ formData, handleReset }, { rejectWithValue }) => {
+    try {
+        const response = await axios.post('/api', formData, { headers: { Authorization: localStorage.getItem('token')}});
+        console.log(response.data);
+        handleReset();
+        return response.data;
+    } catch(err) {
+        console.log(err);
+        return rejectWithValue(err.response.data.error);
+    }
+})
+
+export const removeBooking = createAsyncThunk("booking/removeBooking", async (id, { rejectWithValue }) => {
+    try {
+        const response = await axios.delete('/api', { headers: { Authorization: localStorage.getItem('token')}});
+        console.log(response.data);
+        return response.data;
+    } catch(err) {
+        console.log(err);
+        return err.message;
+    }
+}) 
+
+export const updateBooking = createAsyncThunk("booking/updateBooking", async ({ editId, formData }, { rejectWithValue }) => {
+    try {
+        const response = await axios.put(`/api/`, formData, { headers: { Authorization: localStorage.getItem('token')}});
+        console.log(response.data);
+        handleReset();
+        return response.data;
+    } catch(err) {
+        console.log(err);
+        return rejectWithValue(err.message);
     }
 })
 

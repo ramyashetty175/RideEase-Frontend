c13828ff80a153178026e9547ca9875651c4b841
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2025-12-09 17:37:33 +0530

    changes on vehicleSlice

--- src/slices/vehicleSlice.jsx
+++ src/slices/vehicleSlice.jsx
@@ -12,7 +12,7 @@ export const fetchVehicles = createAsyncThunk("vehicle/fetchVehicles", async(und
 
 export const createVehicle = createAsyncThunk("vehicle/createVehicle", async({ formData, handleReset }, { rejectWithValue }) => {
     try {
-        const response = await axios.post('/api/', formData, { headers: { Authorization: localStorage.getItem('token')}});
+        const response = await axios.post('/api/vehicles', formData, { headers: { Authorization: localStorage.getItem('token')}});
         console.log(response.data);
         handleReset();
         return response.data;
@@ -24,7 +24,7 @@ export const createVehicle = createAsyncThunk("vehicle/createVehicle", async({ f
 
 export const removeVehicle = createAsyncThunk("vehicle/removeVehicle", async (id, { rejectWithValue }) => {
     try {
-        const response = await axios.delete(`/api/vehicle/${id}`, { headers: { Authorization: localStorage.getItem('token')}});
+        const response = await axios.delete(`/api/vehicles/${id}`, { headers: { Authorization: localStorage.getItem('token')}});
         console.log(response.data);
         return response.data;
     } catch(err) {
@@ -35,7 +35,7 @@ export const removeVehicle = createAsyncThunk("vehicle/removeVehicle", async (id
 
 export const updateVehicle = createAsyncThunk("vehicle/updateVehicle", async ({ editId, formData }, { rejectWithValue }) => {
     try {
-        const response = await axios.put(`/api/`, formData, { headers: { Authorization: localStorage.getItem('token')}});
+        const response = await axios.put(`/api/vehicles/${editId}`, formData, { headers: { Authorization: localStorage.getItem('token')}});
         console.log(response.data);
         handleReset();
         return response.data;
@@ -71,7 +71,29 @@ const vehicleSlice = createSlice({
             .addCase(fetchVehicles.fulfilled, (state, action) => {
                 state.data = action.payload;
             })
-            .addCase()
+            .addCase(createVehicle.fulfilled, (state, action) => {
+                state.data.push(action.payload);
+                state.errors = null;
+            })
+            .addCase(createVehicle.rejected, (state, action) => {
+                state.errors = action.payload;
+            })
+            .addCase(removeVehicle.fulfilled, (state, action) => {
+                const idx = state.data.findIndex(ele => ele._id == action.payload._id);
+                state.data.splice(idx, 1);
+            })
+            .addCase(removeVehicle.rejected, (state, action) => {
+                state.errors = action.payload;
+            })
+            .addCase(updateVehicle.fulfilled, (state, action) => {
+                const idx = state.data.findIndex(ele => ele._id == action.payload._id);
+                state.data[idx] = action.payload;
+                state.editId = null;
+            })
+            .addCase(updateVehicle.rejected, (state, action) => {
+                state.errors = action.payload;
+            })
+
     }
 })
 

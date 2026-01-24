8260f386db84cc6aed1dd939b852b6aa219987b9
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2026-01-24 19:32:30 +0530

    Changes on userBooking Component

--- src/pages/UserBookings.jsx
+++ src/pages/UserBookings.jsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 import { useSelector, useDispatch } from "react-redux";
 import { NativeSelect, NativeSelectOption } from "@/components/ui/native-select";
 import { bookingStartTrip, bookingEndTrip } from "../slices/bookingSlice";
@@ -8,62 +7,47 @@ import { SidebarProvider } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar";
 
 export default function UserBookings() {
-  const dispatch = useDispatch();
-  const { data } = useSelector((state) => state.booking);
+    const dispatch = useDispatch();
+    const { data } = useSelector((state) => state.booking);
 
-  const columns = [
-    { accessorKey: "_id", header: "Booking ID" },
-    { accessorKey: "userName", header: "User Name" },
-    { accessorKey: "email", header: "Email" },
-    { accessorKey: "vehicleName", header: "Vehicle" },
-    { accessorKey: "ownerName", header: "Owner" },
-    { accessorKey: "pickupLocation", header: "Pickup" },
-    { accessorKey: "returnLocation", header: "Return" },
-    { accessorKey: "totalAmount", header: "Amount" },
-    { accessorKey: "paymentStatus", header: "Payment" },
-    { accessorKey: "bookingStatus", header: "Status" },
-    {
-      id: "action",
-      header: "Action",
-      cell: ({ row }) => {
+    const columns = [
+      { accessorKey: "_id", header: "Booking ID" },
+      { accessorKey: "userName", header: "User Name" },
+      { accessorKey: "email", header: "Email" },
+      { accessorKey: "vehicleName", header: "Vehicle" },
+      { accessorKey: "ownerName", header: "Owner" },
+      { accessorKey: "pickupLocation", header: "Pickup" },
+      { accessorKey: "returnLocation", header: "Return" },
+      { accessorKey: "totalAmount", header: "Amount" },
+      { accessorKey: "paymentStatus", header: "Payment" },
+      { accessorKey: "bookingStatus", header: "Status" },
+      {
+        id: "action",
+        header: "Action",
+        cell: ({ row }) => {
         const booking = row.original;
         if (!["approved", "in-progress"].includes(booking.bookingStatus)) return null;
-
-        return (
-          <NativeSelect
-            defaultValue=""
+          return (
+            <NativeSelect
             onChange={(e) => {
-  const action = e.target.value;
-  if (action === "start") {
-    dispatch(
-      bookingStartTrip({
-        editId: booking._id,
-        formData: {} 
-      })
-    );
-  }
-
-  if (action === "end") {
-    dispatch(
-      bookingEndTrip({
-        editId: booking._id,
-        formData: {}
-      })
-    );
-  }
-}}
+            const action = e.target.value;
+              if (action === "start") {
+                dispatch(bookingStartTrip({ editId: booking._id }))};
+              if (action === "end") {
+                dispatch(bookingEndTrip({editId: booking._id}))}
+            }}
             className="w-36"
-          >
-            <NativeSelectOption value="" disabled>
+            >
+            <NativeSelectOption value="">
               Select Action
             </NativeSelectOption>
             <NativeSelectOption value="start">Start Trip</NativeSelectOption>
             <NativeSelectOption value="end">End Trip</NativeSelectOption>
           </NativeSelect>
-        );
-      },
-    },
-  ];
+        )
+      }
+    }
+  ]
 
   return (
     <SidebarProvider>

1c88091981b9b89412e4a938a30a5ef8adf7b70e
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2026-05-16 11:15:13 +0530

    changes on create-store

--- src/utils/create-store.js
+++ src/utils/create-store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ownerReducer from "../slices/ownerSlice.jsx";
-import bookingReducer from "../slices/bookingSlice.jsx";
-import vehicleReducer from "../slices/vehicleSlice.jsx";
+import ownerReducer from "../slices/ownerSlice";
+import bookingReducer from "../slices/bookingSlice";
+import vehicleReducer from "../slices/vehicleSlice";
 
 const createStore = () => {
     return configureStore({

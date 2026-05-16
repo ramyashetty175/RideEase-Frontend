8615c3384f47dce74270dbdedca29f61849066df
ramyashetty175 <127672092+ramyashetty175@users.noreply.github.com>
2026-05-16 09:26:17 +0530

    changes on baseURL

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:3020"
+    // baseURL: "http://localhost:3020"
+    baseURL: "https://rideease-backend-zs0m.onrender.com"
 })
